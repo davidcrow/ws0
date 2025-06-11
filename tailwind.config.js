87fdf16b4ad77992afe6566144cd882f6222df50
davidcrow <david@davidcrow.ca>
2025-06-11 15:30:34 -0400

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{html,md,njk}"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
